5821d9cf6d1abe6b81290c5f7abd08b380f0187b
alexander grajales <alex-12-04@hotmail.com>
2017-01-11 09:29:31 -0500

    Add project files.

---
+++ AppArrend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppArrend.MvcApplication" Language="C#" %>
---
+++ AppArrend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppArrend.Startup))]
+namespace AppArrend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
